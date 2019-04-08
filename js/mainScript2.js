11fb70e2fd8b7f31da9ba4b5f3547770b8b8efc1
SmaLLAlien <vlad_belousov1990@hotmail.com>
2019-04-08 23:54:14 +0300

    Add rotation to menu block and reset rotation to default

--- js/mainScript2.js
+++ js/mainScript2.js
@@ -1,187 +1,225 @@
 /**
  * Created by Alien on 18.03.2019.
  */
+document.addEventListener("DOMContentLoaded", init);
 
-let scene = new THREE.Scene();
+
+function init() {
+
+  let scene = new THREE.Scene();
 //scene.background = new THREE.Color( 0xffffff );
-let camera = new THREE.PerspectiveCamera( 75, 300 / 300, 0.1, 1000 );
+  let camera = new THREE.PerspectiveCamera( 75, 300 / 300, 0.1, 1000 );
 
-let renderer = new THREE.WebGLRenderer({ alpha: true });
-renderer.setSize( 300, 300 );
+  let renderer = new THREE.WebGLRenderer({ alpha: true });
+  renderer.setSize( 300, 300 );
 
-let container = document.getElementById("container");
-container.appendChild( renderer.domElement );
+  let container = document.getElementById("container");
+  container.appendChild( renderer.domElement );
 
-let controls = new THREE.OrbitControls(camera, renderer.domElement);
+  let controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-controls.enableZoom = true;
-controls.autoRotate = false;
-let keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
-keyLight.position.set(-300, 0, 300);
+  controls.enableZoom = true;
+  controls.autoRotate = false;
+  let keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
+  keyLight.position.set(-300, 0, 300);
 
-let fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
-fillLight.position.set(300, 0, 300);
+  let fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
+  fillLight.position.set(300, 0, 300);
 
-let backLight = new THREE.DirectionalLight(0xffffff, 1.0);
-backLight.position.set(300, 0, -300).normalize();
+  let backLight = new THREE.DirectionalLight(0xffffff, 1.0);
+  backLight.position.set(300, 0, -300).normalize();
 
-scene.add(keyLight);
-scene.add(fillLight);
-scene.add(backLight);
-
-
-
-camera.position.z = 400;
-
-let mesh;
-
-const loader = new THREE.GLTF2Loader();
-loader.load( 'assets/models/scene.gltf', function ( gltf ) {
-  mesh = gltf.scene;
-
-
-  scene.add( mesh);
+  scene.add(keyLight);
+  scene.add(fillLight);
+  scene.add(backLight);
 
 
 
-  mesh.position.set(10, -30, 1);
-  mesh.rotation.set(0.2, 0, 0);
-  console.log(mesh.rotation);
-} );
+  camera.position.z = 400;
+
+  let mesh;
+
+  const loader = new THREE.GLTF2Loader();
+  loader.load( 'assets/models/scene.gltf', function ( gltf ) {
+    mesh = gltf.scene;
+
+
+    scene.add( mesh);
 
 
 
-function animate() {
-  requestAnimationFrame( animate );
-  controls.update();
-  renderer.render( scene, camera );
+    mesh.position.set(10, -30, 1);
+    mesh.rotation.set(0.2, 0, 0);
+    console.log(mesh.rotation);
+  } );
 
-  //render();
-  container.addEventListener("click", rotor, false);
 
-}
-animate();
 
-/*
- container.addEventListener("click", rotor);
- function rotor(target) {
- mesh.rotation.z = -1;
- mesh.rotation.x = 1;
- //mesh.rotation.x = 180 * 180 / 3.14;
- }
+  function animate() {
+    requestAnimationFrame( animate );
+    controls.update();
+    renderer.render( scene, camera );
 
- */
-/*
-function render() {
-  if(controls.object.position.x !==0) {
-    if(controls.object.position.x > 0 ) {
-      while(controls.object.position.x > 0) {
-        controls.object.position.x -= 0.1;
-      }
-    }
-    else {
-      while(controls.object.position.x < 0) {
-        controls.object.position.y += 0.1;
-      }
-    }
 
+    //render();
+    //container.addEventListener("click", rotor, false);
 
   }
-  if(controls.object.position.y !== 2.45) {
-    if(controls.object.position.y > 2.45 ) {
-      while(controls.object.position.y > 2.45) {
-        controls.object.position.y -= 0.1;
-      }
-    }
-    else {
-      while(controls.object.position.y < 2.45) {
-        controls.object.position.y += 0.1;
-      }
-    }
-  }
-  if(controls.object.position.z !==350) {
-    if(controls.object.position.z > 350 ) {
-      while(controls.object.position.z > 350) {
-        controls.object.position.z -= 0.1;
-      }
+  animate();
 
-    }
-    else {
-      while(controls.object.position.z < 350) {
-        controls.object.position.z += 0.1;
-      }
-    }
-  }
-}
-*/
 
 // ***************************************************************************************************************
 // WORKING WITH DOM
 
+container.classList.toggle("container__animate");
+
 
 // get menu block
-let menu = document.getElementsByClassName("menu")[0];
-let menuChildren = [];
-if(menu.children) {
-  for(let i = 0; i < menu.children.length; i++) {
-    menuChildren[i] = menu.children[i];
-    menuChildren[i].style.opacity = 0;
-  }
-}
-
-// check opacity of menu blocks and change it
-function checkOpacity(elem){
-  if(elem.style.opacity == 0) {
-    console.log(elem.style.visibility, "1");
-     return  elem.style.opacity = 1;
-
-  }
-  else {
-    console.log(elem.style.visibility, "2");
-     return  elem.style.opacity = 0;
-  }
-}
-
-// call change opacity with interval
-function blink(elem) {
-    let t = setInterval(checkOpacity, 70, elem);
-    setTimeout(function(){clearInterval(t)}, 1300);
-}
-
-
-blink(menuChildren[0]);
-blink(menuChildren[1]);
-
-
-let id;
-function rotor(e) {
-  controls.object.position.z = 400;
-  controls.object.position.x = 0;
-  controls.object.position.y = 2.45;
-  //mesh.rotation.set(0.2, 0, 0);
-  mesh.rotation.z += -.05 ;
-  if( mesh.rotation.z <= -1.55) {
-    mesh.rotation.z = -1.55;
-    mesh.rotation.x += .05 ;
-    if( mesh.rotation.x >= 1.3) {
-      mesh.rotation.x = 1.3 ;
-      mesh.rotation.y += .05;
-      if(mesh.rotation.y >= 0.15) {
-        mesh.rotation.y = 0.15;
-        window.cancelAnimationFrame(id);
-        //container.style.position = "relative";
-        //container.style.transition = "transform 3s";
-        //container.transform = "translate(25, 25)"
-      }
+  let menu = document.getElementsByClassName("menu")[0];
+  let menuChildren = [];
+  if(menu.children) {
+    for(let i = 0; i < menu.children.length; i++) {
+      menuChildren[i] = menu.children[i];
+      menuChildren[i].style.opacity = 0;
     }
   }
 
-  id = requestAnimationFrame( rotor );
-  //console.log(mesh.rotation.z, "mesh.rotation.z");
+// check opacity of menu blocks and change it
+  function checkOpacity(elem){
+    if(elem.style.opacity == 0) {
+      return  elem.style.opacity = 1;
+    }
+    else {
+      return  elem.style.opacity = 0;
+    }
+  }
+
+// call change opacity with interval
+  function blink(elem) {
+    let t = setInterval(checkOpacity, 50, elem);
+    setTimeout(function(){clearInterval(t)}, 1000);
+    setTimeout(function(){elem.style.opacity = 1},1050);
+  }
+
+  // CHECK IF USER CHANGE POSITION OBJECT AND RESET IT TO DEFOULT
+  let stopReset;
+  //blink(menuChildren[0]);
+  function resetControls() {
+    stopReset =  requestAnimationFrame( resetControls );
+    if(controls.object.position.z > 400) {
+      controls.object.position.z -= 1;
+      if(controls.object.position.x > 0){
+        controls.object.position.x -= 1;
+        if(controls.object.position.y > 2.45) {
+          controls.object.position.y -= 2.45
+        }
+        else {
+          controls.object.position.y += 2.45
+        }
+      }
+      else {
+        controls.object.position.x += 1;
+        if(controls.object.position.y < 2.45) {
+          controls.object.position.y += 1
+        }
+        else {
+          controls.object.position.y -= 1
+        }
+      }
+
+    }
+    else  {
+      controls.object.position.z += 1;
+      if(controls.object.position.x > 0){
+        controls.object.position.x -= 1;
+        if(controls.object.position.y > 2.45) {
+          controls.object.position.y -= 1
+        }
+        else {
+          controls.object.position.y += 1
+        }
+      }
+      else {
+        controls.object.position.x += 1;
+        if(controls.object.position.y < 2.45) {
+          controls.object.position.y += 1
+        }
+        else {
+          controls.object.position.y -= 1
+        }
+      }
+    }
+    if( (controls.object.position.z <= 401 && controls.object.position.z >= 398) && (controls.object.position.y <= 4 && controls.object.position.y >= 1) && (controls.object.position.x <=2 && controls.object.position.x >=-1 ) ) {
+      //console.log("1");
+      window.cancelAnimationFrame(stopReset);
+
+    }
+
+  }
+
+  // ROTATE OBJECT TO MENU BLOCKS
+  let stopLookRight;
+  function lookingRight(e) {
+    resetControls();
+    //window.cancelAnimationFrame(resetControls);
+    stopLookRight = requestAnimationFrame( lookingRight );
+    mesh.rotation.y += .01;
+    if(mesh.rotation.y > 0.6){
+      mesh.rotation.y = 0.6;
+      blink(menuChildren[1]);
+      window.cancelAnimationFrame(stopLookRight);
+      lookingLeft(e);
+
+    }
+  }
+
+setTimeout(lookingRight,6000, mesh);
+
+  let stopLookLeft;
+  function lookingLeft(e) {
+    stopLookLeft = requestAnimationFrame( lookingLeft );
+    mesh.rotation.y -= .01;
+    if(mesh.rotation.y < -0.6){
+      mesh.rotation.y = -0.6;
+      blink(menuChildren[0]);
+      window.cancelAnimationFrame(stopLookLeft);
+    }
+
+  }
+
+ 
+
+  let id;
+  function rotor(e) {
+    controls.object.position.z = 400;
+    controls.object.position.x = 0;
+    controls.object.position.y = 2.45;
+    //mesh.rotation.set(0.2, 0, 0);
+    mesh.rotation.z += -.05 ;
+    if( mesh.rotation.z <= -1.55) {
+      mesh.rotation.z = -1.55;
+      mesh.rotation.x += .05 ;
+      if( mesh.rotation.x >= 1.3) {
+        mesh.rotation.x = 1.3 ;
+        mesh.rotation.y += .05;
+        if(mesh.rotation.y >= 0.15) {
+          mesh.rotation.y = 0.15;
+          window.cancelAnimationFrame(id);
+          //container.style.position = "relative";
+          //container.style.transition = "transform 3s";
+          //container.transform = "translate(25, 25)"
+        }
+      }
+    }
+
+    id = requestAnimationFrame( rotor );
+    //console.log(mesh.rotation.z, "mesh.rotation.z");
 
 
 
+  }
 }
+
 
 /*
  container.addEventListener("click", rotor);
@@ -218,6 +256,58 @@ function rotor(e) {
 
 
 
+ /*
+ container.addEventListener("click", rotor);
+ function rotor(target) {
+ mesh.rotation.z = -1;
+ mesh.rotation.x = 1;
+ //mesh.rotation.x = 180 * 180 / 3.14;
+ }
+
+ */
+/*
+ function render() {
+ if(controls.object.position.x !==0) {
+ if(controls.object.position.x > 0 ) {
+ while(controls.object.position.x > 0) {
+ controls.object.position.x -= 0.1;
+ }
+ }
+ else {
+ while(controls.object.position.x < 0) {
+ controls.object.position.y += 0.1;
+ }
+ }
+
+
+ }
+ if(controls.object.position.y !== 2.45) {
+ if(controls.object.position.y > 2.45 ) {
+ while(controls.object.position.y > 2.45) {
+ controls.object.position.y -= 0.1;
+ }
+ }
+ else {
+ while(controls.object.position.y < 2.45) {
+ controls.object.position.y += 0.1;
+ }
+ }
+ }
+ if(controls.object.position.z !==350) {
+ if(controls.object.position.z > 350 ) {
+ while(controls.object.position.z > 350) {
+ controls.object.position.z -= 0.1;
+ }
+
+ }
+ else {
+ while(controls.object.position.z < 350) {
+ controls.object.position.z += 0.1;
+ }
+ }
+ }
+ }
+ */
 
 
 
